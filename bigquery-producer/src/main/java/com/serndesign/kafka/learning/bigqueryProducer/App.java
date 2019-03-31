df3d66bdc529afddaf40160dc1148175565eba32
Samuel Neff <sam@serndesign.com>
2019-03-31 12:20:13 -0400

    Update BigQuery query to get a few specific attributes from unnested dataset

--- bigquery-producer/src/main/java/com/serndesign/kafka/learning/bigqueryProducer/App.java
+++ bigquery-producer/src/main/java/com/serndesign/kafka/learning/bigqueryProducer/App.java
@@ -5,7 +5,12 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         var bigquery = BigQueryOptions.getDefaultInstance().getService();
-        var query = "SELECT * FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20160801` LIMIT 10";
+        var query =
+                "select fullVisitorId, hits_unnested.hitNumber, hits_unnested.page.pagePath\n" +
+                "from `bigquery-public-data.google_analytics_sample.ga_sessions_20160801`,\n" +
+                "unnest(hits) as hits_unnested\n" +
+                "order by fullVisitorId, visitNumber, hitNumber\n" +
+                "limit 1000";
         var queryConfig = QueryJobConfiguration.newBuilder(query).build();
         var table = bigquery.query(queryConfig);
 
@@ -28,7 +33,13 @@ public class App {
                 } else {
                     System.out.print(",");
                 }
-                System.out.print(val.toString());
+
+                System.out.print(
+                        val.isNull()
+                                ? "NULL"
+                                : val.getAttribute() == FieldValue.Attribute.PRIMITIVE
+                                    ? val.getStringValue()
+                                    : "COMPLEX");
             }
             System.out.println();
         }
